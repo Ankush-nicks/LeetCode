8e63419467b58505dbe181a7fde9842ef6fb12ab
Ankush kumar Ramteke <112597940+Ankush-nicks@users.noreply.github.com>
2025-07-20 21:56:32 +0530

    Time: 2484 ms (11.43%) | Memory: 13.5 MB (15.90%) - LeetSync

--- Easy/1-two-sum/two-sum.py
+++ Easy/1-two-sum/two-sum.py
@@ -1,8 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}  # Stores number and its index
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0,len(nums)):
+            for j in range(i,len(nums)):
+                if i!=j:
+                    
+                    if nums[i]+nums[j]==target:
+                        return [i,j]
+                    else:
+                        pass
+                else:
+                    pass
+                
